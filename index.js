a44f66974f689c98a6130b7c1413186cbd1ce258
Fernando Dorantes <fernando@merenguemerengue.com>
2019-01-18 12:24:45 -0600

    Changes to be committed: 	modified:   README.md 	deleted:    common/base-api.js 	deleted:    components/localstorage.js 	deleted:    components/memorystorage.js 	modified:   index.js 	modified:   package-lock.json 	modified:   package.json 	deleted:    test/index.js

--- index.js
+++ index.js
@@ -1,4 +1,62 @@
-const LocalStorage = require('./components/localstorage');
-const MemoryStorage = require('./components/memorystorage');
+class AsyncLocalStorage {
 
-module.exports = { LocalStorage, MemoryStorage };
+  getItem(key, defaultValue = null) {
+    return new Promise((resolve, reject) => {
+      if (key in localStorage) {
+        const obj = JSON.parse(localStorage.getItem(key));
+        resolve(obj);
+      }
+      else {
+        reject(defaultValue)
+      }
+    });
+  }
+
+  setItem(key, val) {
+    return new Promise((resolve) => {
+      const item = val;
+      localStorage.setItem(key, JSON.stringify(item));
+      resolve();
+    });
+  }
+
+  removeKey(key) {
+    return new Promise((resolve, reject) => {
+      if (key in localStorage) {
+        localStorage.removeItem(key);
+        resolve()
+      }
+      else {
+        reject();
+      }
+    });
+  }
+
+  has(key) {
+    return new Promise((resolve, reject) => {
+      if (key in localStorage) {
+        const obj = JSON.parse(localStorage.getItem(key));
+        resolve(true);
+      } 
+      else {
+        reject(false);
+      }
+    });
+  }
+
+  clear() {
+    return new Promise((resolve) => {
+      localStorage.clear();
+      resolve();
+    });
+   }
+
+  keys() {
+    return new Promise((resolve) => {
+      resolve(Object.keys(localStorage));  
+    });
+  }
+
+}
+
+module.exports = BaseApi;
